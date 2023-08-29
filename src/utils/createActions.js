30b54abe92004657c07a28a09e556816ad979980
iclae <iclae0@gmail.com>
2023-08-29 15:55:59 +0800

    update Actions

--- src/utils/createActions.js
+++ src/utils/createActions.js
@@ -1,13 +1,14 @@
 /* eslint-disable react/no-multi-comp */
 import React, { Fragment } from 'react';
-import { Divider, Popconfirm } from 'antd';
+import { Divider, Popconfirm, Popover, Menu, Button } from 'antd';
 
 /**
  * @typedef {Object} Action
  * @property {string} text  操作的标题
  * @property {Function} onClick  点击后的回调函数
- * @property {boolean} [popconfirm] 是否使用弹出确认框,为true时poptitle生效
- * @property {string} [poptitle] 弹出确认框的标题
+ * @property {'normal' | 'confirm' | 'more'} [type] 操作的类型,默认为normal
+ * @property {string} [confirmTitle] 弹出确认框的标题
+ * @property {'click' | 'hover'} [moreTrigger] 弹出菜单的触发方式,默认为hover
  * @property {boolean} [hidden] 是否隐藏
  */
 
@@ -17,7 +18,9 @@ import { Divider, Popconfirm } from 'antd';
  * @returns {JSX.Element} <a></a>
  */
 const ActionLink = ({ action: { onClick = () => {}, text } }) => (
-  <a onClick={onClick}>{text}</a>
+  <Button type="link" onClick={onClick}>
+    {text}
+  </Button>
 );
 
 /**
@@ -27,17 +30,45 @@ const ActionLink = ({ action: { onClick = () => {}, text } }) => (
  * @returns {JSX.Element} <Popconfirm><a></a></Popconfirm>
  */
 const ActionPopconfirm = ({
-  action: { onClick = () => {}, text, poptitle },
+  action: { onClick = () => {}, text, confirmTitle },
 }) => (
   <Popconfirm
     cancelText="取消"
     okText="确定"
     onConfirm={onClick}
-    title={<span className="action-pop-mark">{poptitle}</span>}
+    title={confirmTitle}
   >
-    <a>{text}</a>
+    <Button type="link">{text}</Button>
   </Popconfirm>
 );
+
+/**
+ * 返回一个带有更多菜单的按钮
+ * @returns
+ */
+const ActionMore = ({ action }) => {
+  const { moreTrigger = 'hover', text, menus = [] } = action;
+
+  return (
+    <Popover
+      placement="top"
+      content={
+        <div className="folder-more-menu">
+          <Menu>
+            {menus.map((item, index) => (
+              <Menu.Item onClick={item.onClick} key={`${item.text}@${index}`}>
+                {item.text}
+              </Menu.Item>
+            ))}
+          </Menu>
+        </div>
+      }
+      trigger={moreTrigger}
+    >
+      <Button type="link">{text}</Button>
+    </Popover>
+  );
+};
 
 /**
  * 创建操作列
@@ -47,15 +78,25 @@ const ActionPopconfirm = ({
 const createActions = actions =>
   actions
     .filter(action => !action.hidden)
-    .map((action, index) => (
-      <Fragment key={`${action.text}_container`}>
-        {index > 0 && <Divider style={{ margin: '0 6px' }} type="vertical" />}
-        {action.popconfirm ? (
-          <ActionPopconfirm action={action} />
-        ) : (
-          <ActionLink action={action} />
-        )}
-      </Fragment>
-    ));
+    .map((action, index) => {
+      let Action;
+      switch (action.type) {
+        case 'confirm':
+          Action = ActionPopconfirm;
+          break;
+        case 'more':
+          Action = ActionMore;
+          break;
+        default:
+          Action = ActionLink;
+          break;
+      }
+      return (
+        <Fragment key={`${action.text}_container`}>
+          {index > 0 && <Divider style={{ margin: '0 6px' }} type="vertical" />}
+          <Action action={action} />
+        </Fragment>
+      );
+    });
 
 export default createActions;
